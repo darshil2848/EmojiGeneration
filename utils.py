7d4438f68b7e211fa264d73b97589d347130acdf
darshil2848 <49040503+darshil2848@users.noreply.github.com>
2024-10-30 15:22:23 +0530

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+import os
+import pdb
+
+import torch
+from torch import nn
+from torch import  autograd
+import pandas as pd
+
+class Concat_embed(nn.Module):
+
+    def __init__(self, embed_dim, projected_embed_dim):
+        super(Concat_embed, self).__init__()
+        self.projection = nn.Sequential(
+            nn.Linear(in_features=embed_dim, out_features=projected_embed_dim),
+            nn.BatchNorm1d(num_features=projected_embed_dim),
+            nn.LeakyReLU(negative_slope=0.2, inplace=True)
+            )
+
+    def forward(self, inp, embed):
+        projected_embed = self.projection(embed)
+        replicated_embed = projected_embed.repeat(4, 4, 1, 1).permute(2,  3, 0, 1)
+        hidden_concat = torch.cat([inp, replicated_embed], 1)
+
+        return hidden_concat
+
